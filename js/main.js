30159f0d02d8ff8c3002b5ac35b656598e1bdd67
Vladimir Kamuz <v.kamuz@gmail.com>
2022-03-05 21:11:57 +0200

    Filter tasks

--- js/main.js
+++ js/main.js
@@ -16,6 +16,8 @@ function loadEventListeners() {
 	taskList.addEventListener('click', removeTask);
 	// Clear tasks event
 	clearBtn.addEventListener('click', clearTasks);
+	// Filter tasks events
+	filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
@@ -62,4 +64,18 @@ function clearTasks(e){
 	while(taskList.firstChild){
 		taskList.removeChild(taskList.firstChild);
 	}
+}
+
+// Filter tasks
+function filterTasks(e){
+	const text = e.target.value.toLowerCase();
+	document.querySelectorAll('.collection-item').forEach(function(task){
+		// Get text of li item
+		const item = task.firstChild.textContent;
+		if(item.toLowerCase().indexOf(text) !== -1){
+			task.style.display = 'block';
+		} else {
+			task.style.display = 'none';
+		}
+	});
 }
